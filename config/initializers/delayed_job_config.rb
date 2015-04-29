b85b289384bd7c14e659482f6270b65bea01a886
Surya Phaneendra Ganni <ganni.phaneendra@gmail.com>
2015-04-29 08:08:32 +0530

    fixed delayed job initializer

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,5 +1,5 @@
-# Delayed::Worker.destroy_failed_jobs = true
-# Delayed::Worker.max_attempts = 0
+Delayed::Worker.destroy_failed_jobs = true
+Delayed::Worker.max_attempts = 0
 
 # Delayed::Worker.class_eval do
 
@@ -11,3 +11,24 @@
 #   alias_method_chain :handle_failed_job, :notification
 
 # end
+
+# Chain delayed job's handle_failed_job method to do exception notification
+Delayed::Worker.class_eval do
+  def handle_failed_job_with_notification(job, error)
+    handle_failed_job_without_notification(job, error)
+    # only actually send mail in production
+    if Rails.env.production?
+      # rescue if ExceptionNotifier fails for some reason
+      begin
+        ExceptionNotifier.notify_exception(error)
+      rescue Exception => e
+        Rails.logger.error "ExceptionNotifier failed: #{e.class.name}: #{e.message}"
+        e.backtrace.each do |f|
+          Rails.logger.error "  #{f}"
+        end
+        Rails.logger.flush
+      end
+    end
+  end
+  alias_method_chain :handle_failed_job, :notification
+end
