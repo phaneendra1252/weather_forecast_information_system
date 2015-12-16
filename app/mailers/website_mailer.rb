24d3a051075531ca93ed511e33969de0d35b4f53
Surya Phaneendra Ganni <ganni.phaneendra@gmail.com>
2015-12-16 20:10:06 +0530

    changed content

--- app/mailers/website_mailer.rb
+++ app/mailers/website_mailer.rb
@@ -10,13 +10,13 @@ class WebsiteMailer < ActionMailer::Base
     @website = website
     @reports = website.reports(websites)
     parsed_websites = @website.parsed_websites.join(", ")
-    mail(:to => website.report_mail_ids, :subject => "#{Date.today-1} Sucessfully Parsed data for #{parsed_websites}")
+    mail(:to => website.report_mail_ids, :subject => "#{Date.today-1} Sucessfully Parsed data for ... #{parsed_websites}")
   end
 
   def send_errors(website)
     @error_report = website.exception_errors
     @backtrace_errors = website.backtrace_errors
     parsed_websites = website.parsed_websites.join(", ")
-    mail(:to => website.report_mail_ids, :subject => "#{Date.today-1} Error while Parsing #{parsed_websites}")
+    mail(:to => website.report_mail_ids, :subject => "#{Date.today-1} Error while Parsing ... #{parsed_websites}")
   end
 end
